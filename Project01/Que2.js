c11d6dfc173830c2b59bc4c4288a87070d45c32c
Ashik Parmar <ashikparmar5123@gmail.com>
2025-01-17 16:02:50 +0530

    Question2 added

---
+++ Project01/Que2.js
@@ -0,0 +1,25 @@
+/* Que: A 0-indexed array derived with length n is derived by computing the bitwise XOR (⊕) of adjacent values in a binary array original of length n.
+
+Specifically, for each index i in the range [0, n - 1]:
+
+If i = n - 1, then derived[i] = original[i] ⊕ original[0].
+Otherwise, derived[i] = original[i] ⊕ original[i + 1].
+Given an array derived, your task is to determine whether there exists a valid binary array original that could have formed derived.
+
+Return true if such an array exists or false otherwise.
+
+A binary array is an array containing only 0's and 1's
+*/
+
+var doesValidArrayExist = function(derived) {
+    let xr = 0;
+    for(let val of derived){
+        xr = xr^val;
+    }
+    return xr === 0;
+     
+};
+
+const result = doesValidArrayExist([1, 1, 0]);
+
+console.log(result);
