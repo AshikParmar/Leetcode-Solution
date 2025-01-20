a122afd9215f03caa2c3b77360f56dbcdbd9d032
Ashik Parmar <ashikparmar5123@gmail.com>
2025-01-20 15:29:58 +0530

    Que4 and Que5 added

---
+++ Project01/Que4.js
@@ -0,0 +1,15 @@
+/*
+Que:Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+ */
+
+
+let createCounter = function(n) {
+
+    return function() {
+        return n++;
+    };
+};
+
+
+const counter = createCounter(10)       //n=10
+ console.log(counter(),counter(),counter());
