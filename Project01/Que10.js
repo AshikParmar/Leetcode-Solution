d207267524536b2baaf8167d8c923d28a51b702f
Ashik Parmar <ashikparmar5123@gmail.com>
2025-01-21 17:42:54 +0530

    Que11 added

--- Project01/Que10.js
+++ Project01/Que10.js
@@ -1,8 +1,8 @@
 /*
 Que: Given a function fn, return a new function that is identical to the original function except that it ensures fn is called at most once.
 
-The first time the returned function is called, it should return the same result as fn.
-Every subsequent time it is called, it should return undefined. 
+1] The first time the returned function is called, it should return the same result as fn.
+2] Every subsequent time it is called, it should return undefined. 
 */
 
 let once = function(fn) {
